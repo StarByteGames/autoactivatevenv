6f049b65fcf5bc43c0f62882636e1fa74e0a8aad
Thomas Mader <t.mader@eikona.de>
2025-02-12 10:31:15 +0100

    Add clearScreen option to autoactivatevenv configuration

--- src/extension.ts
+++ src/extension.ts
@@ -6,22 +6,35 @@ import * as fs from 'fs';
 export function activate() {
     const config = vscode.workspace.getConfiguration('autoactivatevenv');
     const venvPath = config.get<string>('venvPath', '.venv');
+    const clearScreen = config.get<boolean>('clearScreen', true);
     const workspaceFolder = vscode.workspace.workspaceFolders?.[0].uri.fsPath || '';
     const absoluteVenvPath = path.isAbsolute(venvPath) ? venvPath : path.join(workspaceFolder, venvPath);
     const isVenvExist = fs.existsSync(absoluteVenvPath);
 
-    vscode.window.onDidOpenTerminal((terminal) => {
+    const activateVenvInTerminal = (terminal: vscode.Terminal) => {
         const isWindows = os.platform() === 'win32';
 
         if (isVenvExist) {
             if (isWindows) {
                 sendCommandToTerminal(terminal, `${venvPath}\\Scripts\\activate`);
-                sendCommandToTerminal(terminal, 'cls');
+                if (clearScreen) {
+                    sendCommandToTerminal(terminal, 'cls');
+                }
             } else {
                 sendCommandToTerminal(terminal, `source ${venvPath}/bin/activate`);
-                sendCommandToTerminal(terminal, 'clear');
+                if (clearScreen) {
+                    sendCommandToTerminal(terminal, 'clear');
+                }
             }
         }
+    };
+
+    vscode.window.terminals.forEach(terminal => {
+        activateVenvInTerminal(terminal);
+    });
+
+    vscode.window.onDidOpenTerminal((terminal) => {
+        activateVenvInTerminal(terminal);
     });
 }
 
