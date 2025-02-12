3ad3634e240dcd5d24f3aa948852c7c828bf390d
Thomas Mader <t.mader@eikona.de>
2025-02-12 10:26:06 +0100

    Check if virtual environment exists before activation in terminal

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,26 @@
 import * as vscode from 'vscode';
 import * as os from 'os';
+import * as path from 'path';
+import * as fs from 'fs';
 
 export function activate() {
     const config = vscode.workspace.getConfiguration('autoactivatevenv');
     const venvPath = config.get<string>('venvPath', '.venv');
+    const workspaceFolder = vscode.workspace.workspaceFolders?.[0].uri.fsPath || '';
+    const absoluteVenvPath = path.isAbsolute(venvPath) ? venvPath : path.join(workspaceFolder, venvPath);
+    const isVenvExist = fs.existsSync(absoluteVenvPath);
 
     vscode.window.onDidOpenTerminal((terminal) => {
-        console.log(`New Terminal: ${terminal.name}`);
-        
         const isWindows = os.platform() === 'win32';
-        
-        if (isWindows) {
-            sendCommandToTerminal(terminal, `${venvPath}\\Scripts\\activate`);
-            sendCommandToTerminal(terminal, 'cls');
-        } else {
-            sendCommandToTerminal(terminal, `source ${venvPath}/bin/activate`);
-            sendCommandToTerminal(terminal, 'clear');
+
+        if (isVenvExist) {
+            if (isWindows) {
+                sendCommandToTerminal(terminal, `${venvPath}\\Scripts\\activate`);
+                sendCommandToTerminal(terminal, 'cls');
+            } else {
+                sendCommandToTerminal(terminal, `source ${venvPath}/bin/activate`);
+                sendCommandToTerminal(terminal, 'clear');
+            }
         }
     });
 }
