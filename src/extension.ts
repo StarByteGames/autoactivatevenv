641b54ada2819b6c4d73b1102305d99dd7f204d8
Thomas Mader <t.mader@eikona.de>
2025-02-24 10:01:25 +0100

    code clean up

--- src/extension.ts
+++ src/extension.ts
@@ -6,22 +6,25 @@ import * as fs from 'fs';
 export function activate() {
     const config = vscode.workspace.getConfiguration('autoactivatevenv');
     const venvPath = config.get<string>('venvPath', '.venv');
-    const clearScreen = config.get<boolean>('clearScreen', true);
+    const isWindows = os.platform() === 'win32';
+    
     const workspaceFolder = vscode.workspace.workspaceFolders?.[0].uri.fsPath || '';
     const absoluteVenvPath = path.isAbsolute(venvPath) ? venvPath : path.join(workspaceFolder, venvPath);
     const isVenvExist = fs.existsSync(absoluteVenvPath);
+    
+    const clearScreen = config.get<boolean>('clearScreen', true);
 
     const activateVenvInTerminal = (terminal: vscode.Terminal) => {
-        const isWindows = os.platform() === 'win32';
-
         if (isVenvExist) {
             if (isWindows) {
                 sendCommandToTerminal(terminal, `${venvPath}\\Scripts\\activate`);
+
                 if (clearScreen) {
                     sendCommandToTerminal(terminal, 'cls');
                 }
             } else {
                 sendCommandToTerminal(terminal, `source ${venvPath}/bin/activate`);
+
                 if (clearScreen) {
                     sendCommandToTerminal(terminal, 'clear');
                 }
@@ -39,6 +42,5 @@ export function activate() {
 }
 
 function sendCommandToTerminal(terminal: vscode.Terminal, command: string) {
-    console.log(`Sending command to terminal: ${command}`);
     terminal.sendText(command);
 }
