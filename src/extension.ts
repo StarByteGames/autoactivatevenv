6f05128a991c0f12d3bbcc8ffcca3de87b50583a
Thomas Mader <t.mader@eikona.de>
2025-02-24 16:45:39 +0100

    Update activation events and enhance virtual environment setup on terminal open

--- src/extension.ts
+++ src/extension.ts
@@ -5,24 +5,17 @@ import * as fs from 'fs';
 
 export function activate() {
     const config = vscode.workspace.getConfiguration('autoactivatevenv');
+    const venvPath = config.get<string>('venvPath', '.venv');
     const isWindows = os.platform() === 'win32';
-    
+
     const activateVenvInTerminal = (terminal: vscode.Terminal, clearScreen: boolean) => {
-        const venvPath = config.get<string>('venvPath', '.venv');
-        
         if (fs.existsSync(path.isAbsolute(venvPath) ? venvPath : path.join(vscode.workspace.workspaceFolders?.[0].uri.fsPath || '', venvPath))) {
             if (isWindows) {
                 sendCommandToTerminal(terminal, `${venvPath}\\Scripts\\activate`);
-                
-                if (clearScreen) {
-                    sendCommandToTerminal(terminal, 'cls');
-                }
+                if (clearScreen) {sendCommandToTerminal(terminal, 'cls');}
             } else {
                 sendCommandToTerminal(terminal, `source ${venvPath}/bin/activate`);
-                
-                if (clearScreen) {
-                    sendCommandToTerminal(terminal, 'clear');
-                }
+                if (clearScreen) {sendCommandToTerminal(terminal, 'clear');}
             }
         }
     };
@@ -30,7 +23,24 @@ export function activate() {
     vscode.window.terminals.forEach(terminal => {
         activateVenvInTerminal(terminal, false);
     });
-    
+
+    if (fs.existsSync(path.join(vscode.workspace.workspaceFolders?.[0].uri.fsPath || '', 'requirements.txt')) && 
+        !fs.existsSync(path.isAbsolute(venvPath) ? venvPath : path.join(vscode.workspace.workspaceFolders?.[0].uri.fsPath || '', venvPath))) {
+        
+        vscode.window.showInformationMessage('requirements.txt found! Would you like to install it?', 'Yes', 'No')
+            .then(selection => {
+                if (selection === 'Yes') {
+                    const terminal = vscode.window.createTerminal('Requirements Installer');
+
+                    vscode.window.showInformationMessage('creating virtual environment and installing requirements...');
+                    sendCommandToTerminal(terminal, isWindows ? 'python -m venv .venv' : 'python3 -m venv .venv');
+                    sendCommandToTerminal(terminal, isWindows ? '.venv\\Scripts\\activate' : '.venv/bin/activate');
+                    sendCommandToTerminal(terminal, 'python -m pip install --upgrade pip');
+                    sendCommandToTerminal(terminal, 'pip install -r requirements.txt');
+                }
+        });
+    }
+
     vscode.window.onDidOpenTerminal((terminal) => {
         const config = vscode.workspace.getConfiguration('autoactivatevenv');
         activateVenvInTerminal(terminal, config.get<boolean>('clearScreen', true));
@@ -38,10 +48,10 @@ export function activate() {
 }
 
 function sendCommandToTerminal(terminal: vscode.Terminal, command: string) {
-    terminal.sendText(command);
+        terminal.sendText(command);
 }
 
 // vsce package
 // vsce publish patch
 // vsce publish minor
-// vsce publish major 
+// vsce publish major
